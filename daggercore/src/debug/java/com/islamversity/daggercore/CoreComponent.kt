a80369cca6cdace3c9067506bbca4bfedcf75a25
worldsnas <worldsnas@gmail.com>
2020-07-26 22:38:50 +0200

    initial project structure

---
+++ daggercore/src/debug/java/com/islamversity/daggercore/CoreComponent.kt
@@ -0,0 +1,28 @@
+package com.islamversity.daggercore
+
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.islamversity.daggercore.modules.DatabaseModule
+import com.islamversity.daggercore.modules.network.NetworkModule
+import com.islamversity.daggercore.scope.AppScope
+import com.islamversity.daggercore.modules.domain.DomainModule
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@AppScope
+@Component(
+    modules = [
+        NetworkModule::class,
+        DomainModule::class,
+        DatabaseModule::class,
+        FrescoModule::class]
+)
+interface CoreComponent : BaseComponent{
+
+    fun networkFlipperPlugin(): NetworkFlipperPlugin
+
+    @Component.Builder
+    interface Builder : BaseComponent.Builder {
+        override fun build(): CoreComponent
+    }
+}
