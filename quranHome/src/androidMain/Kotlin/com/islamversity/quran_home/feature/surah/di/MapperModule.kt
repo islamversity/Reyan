e6287e2f1faf41864b8807651d933537e627b020
rezaiyan <alirezaiyann@gmail.com>
2020-09-06 00:33:10 +0430

    The objects which relate to the Surah section provided by Dagger

---
+++ quranHome/src/androidMain/Kotlin/com/islamversity/quran_home/feature/surah/di/MapperModule.kt
@@ -0,0 +1,19 @@
+package com.islamversity.quran_home.feature.surah.di
+
+import com.islamversity.core.Mapper
+import com.islamversity.domain.model.surah.SurahRepoModel
+import com.islamversity.quran_home.feature.surah.mapper.SurahRepoUIMapper
+import com.islamversity.quran_home.feature.surah.model.SurahUIModel
+import dagger.Module
+import dagger.Provides
+
+
+@Module
+object MapperModule {
+
+    @Provides
+    @JvmStatic
+    fun bindUISurahMapper(): Mapper<SurahRepoModel, SurahUIModel> =
+        SurahRepoUIMapper()
+
+}
