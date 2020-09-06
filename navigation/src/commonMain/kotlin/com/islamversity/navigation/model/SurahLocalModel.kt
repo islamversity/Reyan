24fb765d57528e3cd41dbabb2e1ea93b38fb8581
Seyed Jafari <worldsnas@Gmail.com>
2020-09-06 20:15:57 +0200

    added impl for surah presentation layer

--- navigation/src/commonMain/kotlin/com/islamversity/navigation/model/SurahLocalModel.kt
+++ navigation/src/commonMain/kotlin/com/islamversity/navigation/model/SurahLocalModel.kt
@@ -4,13 +4,10 @@ import com.islamversity.navigation.SinkSerializer
 import okio.BufferedSink
 import okio.BufferedSource
 
-
 data class SurahLocalModel(
-    val backTransitionName : String = "",
-    val textTransitionName : String = "",
     val surahID: String,
     val surahName : String,
-    val ayaOrder : Int
+    val startingAyaOrder : Int
 ){
     companion object Sinker : SinkSerializer<SurahLocalModel> {
         const val EXTRA_SURAH_DETAIL = "extra_surah_detail"
@@ -19,18 +16,13 @@ data class SurahLocalModel(
             SurahLocalModel(
                 readUtf8(),
                 readUtf8(),
-                readUtf8(),
-                readUtf8(),
                 readInt()
             )
 
         override fun BufferedSink.writeToSink(obj: SurahLocalModel) {
-            writeUtf8(obj.backTransitionName)
-            writeUtf8(obj.textTransitionName)
             writeUtf8(obj.surahID)
             writeUtf8(obj.surahName)
-            writeInt(obj.ayaOrder)
-
+            writeInt(obj.startingAyaOrder)
         }
     }
 }
--- surah/src/commonMain/kotlin/com/islamversity/surah/SurahProcessor.kt
+++ surah/src/commonMain/kotlin/com/islamversity/surah/SurahProcessor.kt
@@ -1,14 +1,39 @@
 package com.islamversity.surah
 
 import com.islamversity.core.FlowBlock
+import com.islamversity.core.Mapper
+import com.islamversity.core.mapListWith
 import com.islamversity.core.mvi.BaseProcessor
+import com.islamversity.core.ofType
+import com.islamversity.domain.model.aya.AyaRepoModel
+import com.islamversity.domain.model.surah.SurahID
+import com.islamversity.domain.repo.SettingRepo
 import com.islamversity.domain.repo.aya.GetAyaUseCase
 import com.islamversity.navigation.Navigator
+import com.islamversity.surah.model.AyaUIModel
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.map
 
 class SurahProcessor(
     private val navigator: Navigator,
     private val getAyaUseCase: GetAyaUseCase,
+    private val ayaMapper: Mapper<AyaRepoModel, AyaUIModel>,
+    private val settingRepo: SettingRepo,
 ) : BaseProcessor<SurahIntent, SurahResult>() {
 
-    override fun transformers(): List<FlowBlock<SurahIntent, SurahResult>> = emptyList()
+    override fun transformers(): List<FlowBlock<SurahIntent, SurahResult>> = listOf(
+        getMainAyaProcessor,
+    )
+
+    private val getMainAyaProcessor: FlowBlock<SurahIntent, SurahResult> = {
+        ofType<SurahIntent.Initial>()
+            .flatMapMerge {
+                getAyaUseCase.observeAyaMain(SurahID(it.surahId))
+                    .mapListWith(ayaMapper)
+                    .map { ayas ->
+                        val fontSize = settingRepo.getCurrentFontSize()
+                        SurahResult.MainAyasLoaded(ayas, it.startingAyaPosition, fontSize.size.toInt())
+                    }
+            }
+    }
 }
