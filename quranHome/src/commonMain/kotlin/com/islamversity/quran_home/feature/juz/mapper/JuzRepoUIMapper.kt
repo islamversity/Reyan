ecf28d9045bd0c92ee813d53c6ed91547239142c
Ali Rezaiyan <alirezaiyann@gmail.com>
2020-09-24 13:18:10 +0330

    The UI of juz implemented (#42)

--- quranHome/src/commonMain/kotlin/com/islamversity/quran_home/feature/juz/mapper/JuzRepoUIMapper.kt
+++ quranHome/src/commonMain/kotlin/com/islamversity/quran_home/feature/juz/mapper/JuzRepoUIMapper.kt
@@ -7,5 +7,10 @@ import com.islamversity.quran_home.feature.juz.model.JozUIModel
 class JuzRepoUIMapper : Mapper<JuzRepoModel, JozUIModel> {
 
     override fun map(item: JuzRepoModel) =
-        JozUIModel(item.juz)
+        JozUIModel(
+            item.juz,
+            "${item.startingSurahName}: ${item.startingAyaOrder}",
+            "${item.endingSurahName}: ${item.endingAyaOrder}"
+        )
+
 }
