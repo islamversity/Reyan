bd6b2f4dc0beb8a02e67825d87c3d08050cdac01
Seyed Jafari <worldsnas@Gmail.com>
2020-10-10 23:53:49 +0200

    added basic xcode project added core as a shared library fixed a K/N issue with logger class

---
+++ reyanIOS/reyan/Asghar.swift
@@ -0,0 +1,18 @@
+
+import Foundation
+import core
+
+public class Asghar{
+    
+    public init() {
+        
+    }
+    
+    public func first() -> String? {
+    
+        Logger.init()
+        
+        return "Hello"
+    }
+    
+}
---
+++ reyanIOS/reyan/reyanApp.swift
@@ -0,0 +1,17 @@
+//
+//  reyanApp.swift
+//  reyan
+//
+//  Created by Seyed Jafari on 10.10.20.
+//
+
+import SwiftUI
+
+@main
+struct reyanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
