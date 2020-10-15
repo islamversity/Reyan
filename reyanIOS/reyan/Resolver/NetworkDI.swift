d3869f3597b656e572df3641e2695193ce2633a3
meghdad <m.meghdad.fm@gmail.com>
2020-10-15 17:50:19 +0330

    add resolver for DI

---
+++ reyanIOS/reyan/Resolver/NetworkDI.swift
@@ -0,0 +1,29 @@
+
+import Resolver
+
+extension Resolver {
+    public static func registerNetworkServices() {
+
+        
+        
+        
+        
+        
+        // Register protocols XYZFetching and XYZUpdating and create implementation object
+//                register { XYZCombinedService() }
+//                    .scope(application)
+//                    .implements(XYZFetching.self)
+//                    .implements(XYZUpdating.self)
+//
+//                // Register XYZNetworkService and return instance in factory closure
+//                register { XYZNetworkService(session: resolve()) }
+//
+//                // Register XYZSessionService and return instance in factory closure
+//                register { XYZSessionService() }
+//        
+//        
+//        
+        
+        
+    }
+}
