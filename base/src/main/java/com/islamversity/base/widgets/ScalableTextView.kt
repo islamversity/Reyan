46bb646d6e9a97c4bb1d5615791f87669d5eaae0
rezaiyan <alirezaiyann@gmail.com>
2020-11-03 00:13:49 +0330

    ScalableTextView is removed

--- base/src/main/java/com/islamversity/base/widgets/ScalableTextView.kt
+++ base/src/main/java/com/islamversity/base/widgets/ScalableTextView.kt
@@ -2,11 +2,7 @@ package com.islamversity.base.widgets
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.util.AttributeSet
-import android.util.TypedValue
-import android.util.TypedValue.COMPLEX_UNIT_PX
 import androidx.annotation.StringRes
-import androidx.appcompat.widget.AppCompatTextView
 import com.islamversity.base.R
 
 enum class FontSizeScale(
@@ -43,42 +39,5 @@ object AppFontSizeStore {
 
     fun setScale(scale: FontSizeScale) {
         sharedPrefs.edit().putString(KEY_APP_FONT_SIZE_SCALE, scale.raw).apply()
-    }
-}
-
-class ScalableTextView @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    style: Int = 0
-) : AppCompatTextView(context, attrs, style) {
-
-    private val attrFontSize : Float = textSize
-
-    private var fontSizeScale: FontSizeScale = AppFontSizeStore.getCurrentScale()
-
-    private val fontSizeChangeListener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
-        if (key == AppFontSizeStore.KEY_APP_FONT_SIZE_SCALE) {
-            fontSizeScale = AppFontSizeStore.getCurrentScale()
-            setTextSize(COMPLEX_UNIT_PX, attrFontSize)
-        }
-    }
-
-    init {
-        //to set the default stored value
-        setTextSize(COMPLEX_UNIT_PX, attrFontSize)
-    }
-
-    override fun onAttachedToWindow() {
-        super.onAttachedToWindow()
-        AppFontSizeStore.sharedPrefs.registerOnSharedPreferenceChangeListener(fontSizeChangeListener)
-    }
-
-    override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
-        AppFontSizeStore.sharedPrefs.unregisterOnSharedPreferenceChangeListener(fontSizeChangeListener)
-    }
-
-    override fun setTextSize(unit: Int, size: Float) {
-        super.setTextSize(unit, size * fontSizeScale.scale)
     }
 }
