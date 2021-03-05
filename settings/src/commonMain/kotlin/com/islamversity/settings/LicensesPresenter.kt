e1149d82238288ddac5bd529b0d2ff90147fb4ca
neptu39701 <neptu39701@gmail.com>
2021-03-05 21:54:13 +0330

    implemented LicensesPresenter.kt

--- settings/src/commonMain/kotlin/com/islamversity/settings/LicensesPresenter.kt
+++ settings/src/commonMain/kotlin/com/islamversity/settings/LicensesPresenter.kt
@@ -1,7 +1,12 @@
 package com.islamversity.settings
 
+import com.islamversity.core.FlowBlock
 import com.islamversity.core.mvi.BasePresenter
+import com.islamversity.core.mvi.BaseState
 import com.islamversity.core.mvi.MviProcessor
+import com.islamversity.core.notOfType
+import com.islamversity.core.ofType
+import kotlinx.coroutines.flow.take
 
 class LicensesPresenter(
     processor: MviProcessor<LicensesIntent, LicensesResult>
@@ -9,8 +14,27 @@ class LicensesPresenter(
     processor,
     LicensesState.idle()
 ) {
-    override fun reduce(preState: LicensesState, result: LicensesResult): LicensesState {
-        TODO("Not yet implemented")
-    }
+    override fun filterIntent(): List<FlowBlock<LicensesIntent, LicensesIntent>> =
+        listOf(
+            {
+                ofType<LicensesIntent.Initial>().take(1)
+            },
+            {
+                notOfType(LicensesIntent.Initial::class)
+            }
+        )
 
+    override fun reduce(preState: LicensesState, result: LicensesResult): LicensesState =
+        when (result) {
+            is LicensesResult.LastStable ->
+                preState.copy(
+                    base = BaseState.stable()
+                )
+            is LicensesResult.Error ->
+                preState.copy(
+                    base = BaseState.withError(result.err)
+                )
+            is LicensesResult.LicensesList ->
+                preState.copy(licenses = result.list)
+        }
 }
--- settings/src/commonMain/kotlin/com/islamversity/settings/LicensesResult.kt
+++ settings/src/commonMain/kotlin/com/islamversity/settings/LicensesResult.kt
@@ -2,8 +2,11 @@ package com.islamversity.settings
 
 import com.islamversity.core.mvi.BaseState
 import com.islamversity.core.mvi.MviResult
+import com.islamversity.settings.models.LicensesUIModel
 
 sealed class LicensesResult : MviResult {
     object LastStable : LicensesResult()
     class Error(val err: BaseState.ErrorState) : LicensesResult()
+
+    data class LicensesList(val list: List<LicensesUIModel>) : LicensesResult()
 }
