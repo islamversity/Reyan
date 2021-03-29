09155756ce7fb32990bc10a68ae31a5a99beaab8
neptu39701 <neptu39701@gmail.com>
2021-03-29 19:56:36 +0430

    Add Dagger inject to view

--- settings/src/androidMain/kotlin/com/islamversity/settings/LicensesView.kt
+++ settings/src/androidMain/kotlin/com/islamversity/settings/LicensesView.kt
@@ -6,21 +6,25 @@ import com.islamversity.base.CoroutineView
 import com.islamversity.core.mvi.MviPresenter
 import com.islamversity.daggercore.CoreComponent
 import com.islamversity.settings.databinding.ViewLicensesBinding
+import com.islamversity.settings.di.DaggerLicensesComponent
+import com.islamversity.settings.models.LicensesUIModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
-class LicensesView : CoroutineView<ViewLicensesBinding,LicensesState , LicensesIntent>()
-{
+class LicensesView : CoroutineView<ViewLicensesBinding, LicensesState, LicensesIntent>() {
     @Inject
     override lateinit var presenter: MviPresenter<LicensesIntent, LicensesState>
-
+    private var licensesList: List<LicensesUIModel> = emptyList()
     override fun injectDependencies(core: CoreComponent) {
-        TODO("Not yet implemented")
+        DaggerLicensesComponent.factory().create(core).inject(this)
     }
 
     override fun render(state: LicensesState) {
-        TODO("Not yet implemented")
+        renderLoading(state.base)
+        renderError(state.base)
+        licensesList = state.licenses
+
     }
 
     override fun intents(): Flow<LicensesIntent> {
@@ -28,7 +32,7 @@ class LicensesView : CoroutineView<ViewLicensesBinding,LicensesState , LicensesI
     }
 
     override fun bindView(inflater: LayoutInflater, container: ViewGroup): ViewLicensesBinding {
-        TODO("Not yet implemented")
+     return ViewLicensesBinding.inflate(inflater, container, false)
     }
 
 
