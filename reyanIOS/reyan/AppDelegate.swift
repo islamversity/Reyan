88f9c42635dedb983ca838c14c0bb9be62a0b10f
meghdad <m.meghdad.fm@gmail.com>
2021-04-15 22:49:54 +0430

    remove old navigator

--- reyanIOS/reyan/AppDelegate.swift
+++ reyanIOS/reyan/AppDelegate.swift
@@ -11,10 +11,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         setupLogger()
-        // Register modules
-        iOSNavigator.loadRoutableModules()
-        
-        //
         whereIsMySQLite()
                 
         return true
--- reyanIOS/reyan/Navigation/Navigation.swift
+++ reyanIOS/reyan/Navigation/Navigation.swift
@@ -4,15 +4,13 @@ import NavigationRouter
 import nativeShared
 import Resolver
 
-
-
+// global navigator
 public let iOSNavigator : IOSNavigation = IOSNavigation()
 
-public protocol IOSNavigator : Navigator, Resolving{
-    func loadRoutableModules()
-    func getRouter() -> NavigationRouter
-}
+// Navigator type
+public protocol IOSNavigator : Navigator, Resolving{}
 
+// Navigation impl
 public class IOSNavigation : IOSNavigator{
     
     public var root : UINavigationController? = nil
@@ -21,22 +19,8 @@ public class IOSNavigation : IOSNavigator{
         Printer().log(message: "going to \(screen.url)")
         
         let extraData = screen.extras?.second as String?
-//"{\"type\":\"fullSurah\",\"surahName\":\"البقرة\",\"surahID\":\"946431D8-92A2-4375-ABF3-E2D7EE27999A\",\"startingAyaOrder\":0}"
-//        root!.pushViewController(UIHostingController(rootView: SurahView(presenter: resolver.resolve(), initialData: SurahLocalModel.FullSurah(surahName: "testing", surahID: "946431D8-92A2-4375-ABF3-E2D7EE27999A", startingAyaOrder: 0))), animated: true)
-//        if screen is Screens.Home{
-//            root!.pushViewController(Creator().create(), animated: true)
-//        }else{
-//        if extraData == nil {
-//            NavigationRouter.main.navigate(toPath: screen.url)
-//        }else{
-//            let baseExtra = extraData!.base16EncodedString
-////            print("path=\(screen.url + "/" + baseExtra)")
-//            NavigationRouter.main.navigate(toPath: screen.url + "/" + baseExtra)
-//        }
-//        }
         
         if screen is Screens.Home {
-//            root!.popViewController(animated: false)
             
             root!.pushViewController(UIHostingController(rootView: QuranHomeView(presenter: resolver.resolve())), animated: false)
             
@@ -55,31 +39,5 @@ public class IOSNavigation : IOSNavigator{
             root!.pushViewController(UIHostingController(
             rootView: HomeSettingsView()), animated: true)
         }
-        
-        
     }
-    
-    public func loadRoutableModules () {
-        RoutableModulesFactory.loadRoutableModules()
-    }
-    
-    public func getRouter() -> NavigationRouter {
-        NavigationRouter.main
-    }
-    
-//    private func createController(screen : Screens) -> UIHostingController<_> {
-//        if case Screens.Home = screen {
-//            return
-//        }
-//    }
 }
-
-//let encoder = JSONEncoder()
-//       encoder.outputFormatting = .sortedKeys //.prettyPrinted
-//       let payload: Data? = try? encoder.encode(initialModel)
-//       path = ScreenURL(urlString: "/address/upsert/\(String(data: payload ?? Data(), encoding: .utf8)!)")
-//       
-
-
-
-
