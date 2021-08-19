040a71c0d638e42fd23a7581ca60af7f9e604b4a
rezaiyan <alirezaiyann@gmail.com>
2021-08-19 12:16:51 +0430

    Added commonTest dependencies

---
+++ domain/src/commonTest/kotlin/com/islamversity/comain/usecase/DatabaseFillerUseCaseTest.kt
@@ -0,0 +1,18 @@
+package com.islamversity.comain.usecase
+
+import com.islamversity.domain.usecase.DatabaseFillerUseCase
+import io.mockk.mockk
+import kotlin.test.Asserter
+import kotlin.test.Test
+import kotlinx.coroutines.*
+import kotlin.test.assertTrue
+
+class DatabaseFillerUseCaseTest {
+
+    val it = mockk<DatabaseFillerUseCase>()
+
+    @Test
+    fun firstTest(){
+    }
+
+}
